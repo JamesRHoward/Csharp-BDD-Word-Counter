f9a12a496d91c057733824e7b2b1f8ecc2c4573b
James Howard <james@>
2016-07-08 13:17:55 -0700

    second attempt at project. Created project folder containing folders for views, objects, modules, and tests. files added to respective folders and readme.md created

---
+++ Startup.cs
@@ -0,0 +1,36 @@
+using Nancy.ViewEngines.Razor;
+
+namespace WordRepeat
+{
+  public class Startup
+  {
+    public void Configure(IApplicationBuilder app)
+    {
+      app.UseOwin(x => x.UseNancy());
+    }
+  }
+  public class CustomRootPathProvider : IRootPathProvider
+  {
+    public string GetRootPath()
+    {
+      return Directory.GetCurrentDirectory();
+    }
+  }
+  public class RazorConfig : IRazorConfiguration
+  {
+    public IEnumerable<string> GetAssemblyNames()
+    {
+      return null;
+    }
+
+    public IEnumerable<string> GetDefaultNamespaces()
+    {
+      return null;
+    }
+
+    public bool AutoIncludeModelNamespace
+    {
+      get { return false; }
+    }
+  }
+}
