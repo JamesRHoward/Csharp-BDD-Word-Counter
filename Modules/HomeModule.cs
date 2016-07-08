23c58791c15609bf3e9a42f3c8e3e9a635872378
James Howard <james@>
2016-07-08 14:01:53 -0700

    homemodule built. will add view for results.

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,21 @@
+using Nancy;
+using System;
+using System.Collections.Generic;
+
+namespace RepeatCounter
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        return View["index.cshtml"];
+      };
+      Post["/result"] = _ => {
+        RepeatCounter newCount = new RepeatCounter();
+        var NumberOfWords = newCount.CountRepeats(Request.Form["sentence"],Request.Form["word"]);
+        return View["results.cshtml", NumberOfWords];
+      }
+    }
+  }
+}
